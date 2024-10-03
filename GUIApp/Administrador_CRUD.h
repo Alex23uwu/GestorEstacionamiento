27adc4ac9ef3e0ae4f916fdff06edb820aea3151
NVega-code <a20222274@pucp.edu.pe>
2024-10-03 13:54:14 -0500

    enlace a ventana administrador

--- GUIApp/Administrador_CRUD.h
+++ GUIApp/Administrador_CRUD.h
@@ -48,11 +48,17 @@ namespace GUIApp {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Administrador_CRUD";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->SuspendLayout();
+			// 
+			// Administrador_CRUD
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(584, 429);
+			this->Name = L"Administrador_CRUD";
+			this->Text = L"Administrador_CRUD";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
 	};
--- GUIApp/MyForm.h
+++ GUIApp/MyForm.h
@@ -1,8 +1,7 @@
 #pragma once
 #include "PersonalLimpieza_CRUD.h"
 #include "Veedor_CRUD.h"
-#include "Cliente_CRUD.h"
-/*#include "Administrador_CRUD.h"*/
+#include "Administrador_CRUD.h"
 
 namespace GUIApp {
 
@@ -56,7 +55,6 @@ namespace GUIApp {
 
 	private: System::Windows::Forms::ToolStripMenuItem^ veedoresToolStripMenuItem;
 	private: System::Windows::Forms::ToolStripMenuItem^ administradoresToolStripMenuItem;
-	private: System::Windows::Forms::ToolStripMenuItem^ clientesToolStripMenuItem;
 
 	protected:
 
@@ -82,24 +80,22 @@ namespace GUIApp {
 			this->mantenimientoToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->personalLimpiezaToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->veedoresToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
-			this->administradoresToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->transaccionesToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->reporteToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->ayudaToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
-			this->clientesToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->administradoresToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->menuStrip1->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// menuStrip1
 			// 
-			this->menuStrip1->ImageScalingSize = System::Drawing::Size(20, 20);
 			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(5) {
 				this->archivoToolStripMenuItem,
 					this->mantenimientoToolStripMenuItem, this->transaccionesToolStripMenuItem, this->reporteToolStripMenuItem, this->ayudaToolStripMenuItem
 			});
 			this->menuStrip1->Location = System::Drawing::Point(0, 0);
 			this->menuStrip1->Name = L"menuStrip1";
-			this->menuStrip1->Size = System::Drawing::Size(904, 28);
+			this->menuStrip1->Size = System::Drawing::Size(678, 24);
 			this->menuStrip1->TabIndex = 0;
 			this->menuStrip1->Text = L"menuStrip1";
 			// 
@@ -110,97 +106,88 @@ namespace GUIApp {
 					this->guardarCambiosToolStripMenuItem, this->salirToolStripMenuItem
 			});
 			this->archivoToolStripMenuItem->Name = L"archivoToolStripMenuItem";
-			this->archivoToolStripMenuItem->Size = System::Drawing::Size(73, 24);
+			this->archivoToolStripMenuItem->Size = System::Drawing::Size(60, 20);
 			this->archivoToolStripMenuItem->Text = L"Archivo";
 			// 
 			// historialToolStripMenuItem
 			// 
 			this->historialToolStripMenuItem->Name = L"historialToolStripMenuItem";
-			this->historialToolStripMenuItem->Size = System::Drawing::Size(205, 26);
+			this->historialToolStripMenuItem->Size = System::Drawing::Size(164, 22);
 			this->historialToolStripMenuItem->Text = L"Historial";
 			// 
 			// guardarCambiosToolStripMenuItem
 			// 
 			this->guardarCambiosToolStripMenuItem->Name = L"guardarCambiosToolStripMenuItem";
-			this->guardarCambiosToolStripMenuItem->Size = System::Drawing::Size(205, 26);
+			this->guardarCambiosToolStripMenuItem->Size = System::Drawing::Size(164, 22);
 			this->guardarCambiosToolStripMenuItem->Text = L"Guardar cambios";
 			// 
 			// salirToolStripMenuItem
 			// 
 			this->salirToolStripMenuItem->Name = L"salirToolStripMenuItem";
-			this->salirToolStripMenuItem->Size = System::Drawing::Size(205, 26);
+			this->salirToolStripMenuItem->Size = System::Drawing::Size(164, 22);
 			this->salirToolStripMenuItem->Text = L"Salir";
 			this->salirToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::salirToolStripMenuItem_Click);
 			// 
 			// mantenimientoToolStripMenuItem
 			// 
-			this->mantenimientoToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(4) {
+			this->mantenimientoToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(3) {
 				this->personalLimpiezaToolStripMenuItem,
-					this->veedoresToolStripMenuItem, this->administradoresToolStripMenuItem, this->clientesToolStripMenuItem
+					this->veedoresToolStripMenuItem, this->administradoresToolStripMenuItem
 			});
 			this->mantenimientoToolStripMenuItem->Name = L"mantenimientoToolStripMenuItem";
-			this->mantenimientoToolStripMenuItem->Size = System::Drawing::Size(124, 24);
+			this->mantenimientoToolStripMenuItem->Size = System::Drawing::Size(101, 20);
 			this->mantenimientoToolStripMenuItem->Text = L"Mantenimiento";
 			// 
 			// personalLimpiezaToolStripMenuItem
 			// 
 			this->personalLimpiezaToolStripMenuItem->Name = L"personalLimpiezaToolStripMenuItem";
-			this->personalLimpiezaToolStripMenuItem->Size = System::Drawing::Size(232, 26);
+			this->personalLimpiezaToolStripMenuItem->Size = System::Drawing::Size(185, 22);
 			this->personalLimpiezaToolStripMenuItem->Text = L"Personal de Limpieza";
 			this->personalLimpiezaToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::personalLimpiezaToolStripMenuItem_Click);
 			// 
 			// veedoresToolStripMenuItem
 			// 
 			this->veedoresToolStripMenuItem->Name = L"veedoresToolStripMenuItem";
-			this->veedoresToolStripMenuItem->Size = System::Drawing::Size(232, 26);
+			this->veedoresToolStripMenuItem->Size = System::Drawing::Size(185, 22);
 			this->veedoresToolStripMenuItem->Text = L"Veedores";
 			this->veedoresToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::veedoresToolStripMenuItem_Click);
-			// 
-			// administradoresToolStripMenuItem
-			// 
-			this->administradoresToolStripMenuItem->Name = L"administradoresToolStripMenuItem";
-			this->administradoresToolStripMenuItem->Size = System::Drawing::Size(232, 26);
-			this->administradoresToolStripMenuItem->Text = L"Administradores";
-			this->administradoresToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::administradoresToolStripMenuItem_Click);
 			// 
 			// transaccionesToolStripMenuItem
 			// 
 			this->transaccionesToolStripMenuItem->Name = L"transaccionesToolStripMenuItem";
-			this->transaccionesToolStripMenuItem->Size = System::Drawing::Size(114, 24);
+			this->transaccionesToolStripMenuItem->Size = System::Drawing::Size(92, 20);
 			this->transaccionesToolStripMenuItem->Text = L"Transacciones";
 			// 
 			// reporteToolStripMenuItem
 			// 
 			this->reporteToolStripMenuItem->Name = L"reporteToolStripMenuItem";
-			this->reporteToolStripMenuItem->Size = System::Drawing::Size(76, 24);
+			this->reporteToolStripMenuItem->Size = System::Drawing::Size(60, 20);
 			this->reporteToolStripMenuItem->Text = L"Reporte";
 			// 
 			// ayudaToolStripMenuItem
 			// 
 			this->ayudaToolStripMenuItem->Name = L"ayudaToolStripMenuItem";
-			this->ayudaToolStripMenuItem->Size = System::Drawing::Size(65, 24);
+			this->ayudaToolStripMenuItem->Size = System::Drawing::Size(53, 20);
 			this->ayudaToolStripMenuItem->Text = L"Ayuda";
 			// 
-			// clientesToolStripMenuItem
+			// administradoresToolStripMenuItem
 			// 
-			this->clientesToolStripMenuItem->Name = L"clientesToolStripMenuItem";
-			this->clientesToolStripMenuItem->Size = System::Drawing::Size(232, 26);
-			this->clientesToolStripMenuItem->Text = L"Clientes";
-			this->clientesToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::clientesToolStripMenuItem_Click);
+			this->administradoresToolStripMenuItem->Name = L"administradoresToolStripMenuItem";
+			this->administradoresToolStripMenuItem->Size = System::Drawing::Size(185, 22);
+			this->administradoresToolStripMenuItem->Text = L"Administradores";
+			this->administradoresToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::administradoresToolStripMenuItem_Click);
 			// 
 			// MyForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::SystemColors::ActiveCaption;
-			this->ClientSize = System::Drawing::Size(904, 591);
+			this->ClientSize = System::Drawing::Size(678, 480);
 			this->Controls->Add(this->menuStrip1);
 			this->IsMdiContainer = true;
 			this->MainMenuStrip = this->menuStrip1;
-			this->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->Name = L"MyForm";
 			this->Text = L"Sistema de Estacionamiento";
-			this->WindowState = System::Windows::Forms::FormWindowState::Maximized;
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
 			this->menuStrip1->ResumeLayout(false);
 			this->menuStrip1->PerformLayout();
@@ -230,10 +217,7 @@ private: System::Void veedoresToolStripMenuItem_Click(System::Object^ sender, Sy
 }
 
 private: System::Void administradoresToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e) {
-	
-}
-private: System::Void clientesToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e) {
-	Cliente_CRUD^ form = gcnew Cliente_CRUD();
+	Administrador_CRUD^ form = gcnew Administrador_CRUD();
 	form->MdiParent = this;
 	form->Show();
 }
