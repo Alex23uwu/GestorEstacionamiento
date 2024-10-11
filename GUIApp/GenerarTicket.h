bd0440aa90c9e4b5f6802e358484f7e517c982ee
NVega-code <a20222274@pucp.edu.pe>
2024-10-10 21:21:55 -0500

    Ticket actualizado

--- GUIApp/GenerarTicket.h
+++ GUIApp/GenerarTicket.h
@@ -37,12 +37,16 @@ namespace GUIApp {
 	private: System::Windows::Forms::Label^ label1;
 	private: System::Windows::Forms::TextBox^ txtPlacaVehiculo;
 	private: System::Windows::Forms::Label^ label2;
-	private: System::Windows::Forms::Label^ lblHora;
+
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::Label^ label3;
-	private: System::Windows::Forms::Timer^ timer1;
+
 	private: System::Windows::Forms::TextBox^ txtHoraEntrada;
 	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox3;
 	private: System::ComponentModel::IContainer^ components;
 
 	protected:
@@ -60,16 +64,17 @@ namespace GUIApp {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = (gcnew System::ComponentModel::Container());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->txtPlacaVehiculo = (gcnew System::Windows::Forms::TextBox());
 			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->lblHora = (gcnew System::Windows::Forms::Label());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->label3 = (gcnew System::Windows::Forms::Label());
-			this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
 			this->txtHoraEntrada = (gcnew System::Windows::Forms::TextBox());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
 			this->SuspendLayout();
 			// 
 			// label1
@@ -97,51 +102,73 @@ namespace GUIApp {
 			this->label2->TabIndex = 2;
 			this->label2->Text = L"Hora de Entrada";
 			// 
-			// lblHora
-			// 
-			this->lblHora->AutoSize = true;
-			this->lblHora->Location = System::Drawing::Point(41, 122);
-			this->lblHora->Name = L"lblHora";
-			this->lblHora->Size = System::Drawing::Size(60, 13);
-			this->lblHora->TabIndex = 3;
-			this->lblHora->Text = L"HoraActual";
-			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(362, 114);
+			this->button1->Location = System::Drawing::Point(349, 212);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(124, 29);
 			this->button1->TabIndex = 4;
-			this->button1->Text = L"Generar";
+			this->button1->Text = L"Registrar";
 			this->button1->UseVisualStyleBackColor = true;
 			// 
 			// label3
 			// 
 			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(41, 156);
+			this->label3->Location = System::Drawing::Point(41, 191);
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(59, 13);
 			this->label3->TabIndex = 5;
 			this->label3->Text = L"Pago Total";
 			// 
-			// timer1
-			// 
-			this->timer1->Enabled = true;
-			this->timer1->Tick += gcnew System::EventHandler(this, &GenerarTicket::timer1_Tick);
-			// 
 			// txtHoraEntrada
 			// 
 			this->txtHoraEntrada->Location = System::Drawing::Point(162, 81);
 			this->txtHoraEntrada->Name = L"txtHoraEntrada";
+			this->txtHoraEntrada->ReadOnly = true;
 			this->txtHoraEntrada->Size = System::Drawing::Size(100, 20);
 			this->txtHoraEntrada->TabIndex = 6;
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(162, 153);
+			this->textBox1->Location = System::Drawing::Point(162, 188);
 			this->textBox1->Name = L"textBox1";
+			this->textBox1->ReadOnly = true;
 			this->textBox1->Size = System::Drawing::Size(100, 20);
 			this->textBox1->TabIndex = 7;
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(41, 122);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(77, 13);
+			this->label4->TabIndex = 8;
+			this->label4->Text = L"Hora de Salida";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(41, 158);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(97, 13);
+			this->label5->TabIndex = 9;
+			this->label5->Text = L"Tiempo Consumido";
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(162, 119);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->ReadOnly = true;
+			this->textBox2->Size = System::Drawing::Size(100, 20);
+			this->textBox2->TabIndex = 10;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(162, 155);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->ReadOnly = true;
+			this->textBox3->Size = System::Drawing::Size(100, 20);
+			this->textBox3->TabIndex = 11;
 			// 
 			// GenerarTicket
 			// 
@@ -149,11 +176,14 @@ namespace GUIApp {
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::SystemColors::ActiveCaption;
 			this->ClientSize = System::Drawing::Size(531, 425);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->label4);
 			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->txtHoraEntrada);
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->button1);
-			this->Controls->Add(this->lblHora);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->txtPlacaVehiculo);
 			this->Controls->Add(this->label1);
